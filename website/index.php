34186a842fe21e8cf83e0cce7505d9eefbbe3a33
pankajcode01 <111601014@smail.iitpkd.ac.in>
2019-02-25 19:15:44 +0530

    show tables and insert form

--- website/index.php
+++ website/index.php
@@ -29,12 +29,44 @@ require('config.php');
   <a href=''  color="red" class='logo'>My SUPERMARKET</a>
  
   <div class='header-right'>
-    <a class='active' href='index.php'>Products</a>
+    <a class='active' href='index.php'>SHOW TABLES</a>
     <a href='insert.php'>Insert</a>
   </div>
 </div>
+<form action = '#' method = 'post'>
 
-<table id='t01'>
+ select TABLE:        
+         <select name = 'table'>
+            <option value = 'bill'>BILL</option>
+            <option value = 'clothes'>CLOTHES</option>
+            <option value = 'customer'>CUSTOMER</option>
+            <option value = 'department'>DEPARTMENT</option>
+            <option value = 'discount'>DISCOUNT</option>
+            <option value = 'electronics'>ELECTRONICS</option>
+            <option value = 'employee'>EMPLOYEE</option>
+            <option value = 'foodSection'>FOOD SECTION</option>
+            <option value = 'household'>HOUSEHOLD</option>
+            <option value = 'offers'>OFFERS</option>
+            <option value = 'ordered'>ORDERED</option>
+            <option value = 'product'>PRODUCT</option>
+            <option value = 'sales'>SALES</option>
+            <option value = 'sports'>SPORTS</option>
+            <option value = 'supplied'>SUPPLIED</option>
+            <option value = 'supplier'>SUPPLIER</option>
+         </select>
+         <input type = 'submit' value = 'SHOW' />
+      </form>
+
+
+  
+<?php
+    if (isset($_REQUEST['table'])){
+      $table_name = stripslashes($_REQUEST['table']); // removes backslashes
+      
+     
+      if($table_name=='product')
+      {
+     echo "  <table id='t01'>
   <tr>
       <th>ProductID</th>
     <th>ProductName</th>
@@ -43,10 +75,7 @@ require('config.php');
     <th>Costprice</th>
     <th>MRP</th>
     <th>Quantity</th>
-  </tr>
-  
-<?php
-    
+  </tr>";
           $sql = "SELECT * FROM product";
       
         $run_query = mysqli_query($con,$sql);
@@ -77,5 +106,562 @@ require('config.php');
    
   
   echo"</table>";
+}
+
+/* second table */
+
+if($table_name=='clothes')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Product ID</th>
+<th>Category</th>
+<th>Size</th>
+<th>Season</th>
+</tr>";
+    $sql = "SELECT * FROM clothes";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$productID  = $row['productID'];
+$category  = $row['category'];
+$size = $row['size'];
+$season = $row['season'];
+    
+echo "
+<tr>
+<td>$productID</td>
+<td>$category</td>
+<td>$size</td>
+<td>$season</td>
+</tr>
+";
+}
+}
+
+
+
+echo"</table>";
+}
+/* third table  customer*/
+
+if($table_name=='customer')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Customer ID</th>
+<th>Customer Name</th>
+<th>Phone No</th>
+<th>Customer Address</th>
+<th>email Id </th>
+<th>Join Date </th>
+<th> Money Spent </th>
+</tr>";
+    $sql = "SELECT * FROM customer";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['customerID'];
+$r2 = $row['customerName'];
+$r3 = $row['phoneNo'];
+$r4= $row['customerAddress'];
+$r5= $row['emailID'];
+$r6 = $row['joinDate'];
+$r7= $row['moneySpent'];
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+<td>$r4</td>
+<td>$r5</td>
+<td>$r6</td>
+<td>$r7</td>
+</tr>
+";
+}
+}
+
+
+
+echo"</table>";
+}
+/*fourth table department*/
+
+
+if($table_name=='department')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Department Name</th>
+<th>Manager ID</th>
+<th>Vacancy</th>
+
+</tr>";
+    $sql = "SELECT * FROM department";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['departmentName'];
+$r2 = $row['managerID'];
+$r3 = $row['vacancy'];
+
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+
+</tr>
+";
+}
+}
+
+
+
+echo"</table>";
+}
+
+/* table 5 discount */
+
+if($table_name=='discount')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Discount ID</th>
+<th>Amount</th>
+<th>Discount Percent</th>
+<th>Valid Upto</th>
+<th>Details</th>
+<th>Terms And Conditions</th>
+</tr>";
+    $sql = "SELECT * FROM discount";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['discountID'];
+$r2 = $row['amount'];
+$r4= $row['discountPercent'];
+$r5= $row['validUpto'];
+$r6 = $row['details'];
+$r7= $row['termsAndConditions'];
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r4</td>
+<td>$r5</td>
+<td>$r6</td>
+<td>$r7</td>
+</tr>
+";
+}
+}
+
+echo"</table>";
+}
+
+/* table 6 electronics */
+
+if($table_name=='electronics')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Product ID</th>
+<th>PowerRating</th>
+<th>Warranty</th>
+<th>details</th>
+</tr>";
+    $sql = "SELECT * FROM electronics";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['productID'];
+$r2 = $row['powerRating'];
+$r3 = $row['warranty'];
+$r4= $row['details'];
+
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+<td>$r4</td>
+
+</tr>
+";
+}
+}
+echo"</table>";
+}
+
+/* new table 7 employee */
+if($table_name=='employee')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Employee ID</th>
+<th>Employee Name</th>
+<th>DOB</th>
+<th>Phone No</th>
+<th>Employee Address </th>
+<th>Email ID</th>
+<th>Join Date </th>
+<th>Salary</th>
+<th>Department Name</th>
+</tr>";
+    $sql = "SELECT * FROM employee";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['employeeID'];
+$r2 = $row['employeeName'];
+$r3 = $row['DOB'];
+$r4= $row['phoneNo'];
+$r5= $row['employeeAddress'];
+$r6 = $row['emailID'];
+$r7= $row['joinDate'];
+$r8= $row['salary'];
+$r9= $row['departmentName'];
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+<td>$r4</td>
+<td>$r5</td>
+<td>$r6</td>
+<td>$r7</td>
+<td>$r8</td>
+<td>$r9</td>
+</tr>
+";
+}
+}
+echo"</table>";
+}
+
+/* table 8  food section */
+
+
+if($table_name=='foodSection')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Product ID</th>
+<th>Manufacture Date</th>
+<th>Expiry Date</th>
+<th>Quantity</th>
+</tr>";
+    $sql = "SELECT * FROM foodSection";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['productID'];
+$r2 = $row['manufactureDate'];
+$r3 = $row['expiryDate'];
+$r4= $row['quantity'];
+
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+<td>$r4</td>
+
+</tr>
+";
+}
+}
+
+
+
+echo"</table>";
+}
+
+/* 9 table  household*/
+
+if($table_name=='household')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Product ID</th>
+<th>Quantity</th>
+<th>Category</th>
+</tr>";
+    $sql = "SELECT * FROM household";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['productID'];
+$r2 = $row['quantity'];
+$r3 = $row['category'];
+
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+
+</tr>
+";
+}
+}
+echo"</table>";
+}
+/* 11 table  offers*/
+
+if($table_name=='offers')
+{
+echo "  <table id='t01'>
+<tr>
+<th>discount ID</th>
+<th>product ID</th>
+
+</tr>";
+    $sql = "SELECT * FROM offers";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['discountID'];
+$r2 = $row['productID'];
+
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+</tr>
+";
+}
+}
+echo"</table>";
+}
+
+/* 12 table  ordered*/
+
+if($table_name=='ordered')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Order ID</th>
+<th>Supplier ID</th>
+<th>Product ID</th>
+<th>order Date</th>
+<th>Quantity Ordered</th>
+
+</tr>";
+    $sql = "SELECT * FROM ordered";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['orderID'];
+$r2 = $row['supplierID'];
+$r3 = $row['productID'];
+$r4= $row['orderDate'];
+$r5= $row['quantityOrdered'];
+
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+<td>$r4</td>
+<td>$r5</td>
+
+</tr>
+";
+}
+}
+
+echo"</table>";
+}
+
+/* 13 table  sales*/
+
+if($table_name=='sales')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Bill NO</th>
+<th>ProductID</th>
+<th>Discount ID</th>
+<th>Quantity sold</th>
+</tr>";
+    $sql = "SELECT * FROM sales";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['billNo'];
+$r2 = $row['productID'];
+$r3 = $row['discountID'];
+$r4= $row['quantitySold'];
+
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+<td>$r4</td>
+
+</tr>
+";
+}
+}
+
+echo"</table>";
+}
+
+/* 14 table  sports*/
+
+if($table_name=='sports')
+{
+echo "  <table id='t01'>
+<tr>
+<th> Product ID</th>
+<th>Warranty</th>
+<th>Details</th>
+<th>Category</th>
+
+</tr>";
+    $sql = "SELECT * FROM sports";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['productID'];
+$r2 = $row['warranty'];
+$r3 = $row['details'];
+$r4= $row['category'];
+
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+<td>$r4</td>
+
+</tr>
+";
+}
+}
+
+echo"</table>";
+}
+
+/* 15 table  supply*/
+
+if($table_name=='supplied')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Supply ID</th>
+<th>Order ID</th>
+<th>Supply Date</th>
+<th>Quantity Supplied</th>
+
+</tr>";
+    $sql = "SELECT * FROM supplied";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['supplyID'];
+$r2 = $row['orderID'];
+$r3 = $row['supplyDate'];
+$r4= $row['quantitySupplied'];
+
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+<td>$r4</td>
+
+</tr>
+";
+}
+}
+
+
+
+echo"</table>";
+}
+
+/* 16 table  supplier*/
+
+if($table_name=='supplier')
+{
+echo "  <table id='t01'>
+<tr>
+<th>Supplier ID ID</th>
+<th>Supplier Name</th>
+<th>Phone No</th>
+<th>Supplier Address</th>
+<th>email Id </th>
+
+</tr>";
+    $sql = "SELECT * FROM supplier";
+
+  $run_query = mysqli_query($con,$sql);
+if(mysqli_num_rows($run_query) > 0){
+while($row = mysqli_fetch_array($run_query)){
+$r1 = $row['supplierID'];
+$r2 = $row['supplierName'];
+$r3 = $row['phoneNo'];
+$r4= $row['supplierAddress'];
+$r5= $row['emailID'];
+
+    
+    
+echo "
+<tr>
+<td>$r1</td>
+<td>$r2</td>
+<td>$r3</td>
+<td>$r4</td>
+<td>$r5</td>
+
+</tr>
+";
+}
+}
+
+
+
+echo"</table>";
+}
+    }
       ?>
    
---
+++ website/product.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta charset="utf-8"> 
+  </head>
+<title>My Supermarket</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+
+
+<div class='header'>
+  <a href=''  color="red" class='logo'>My SUPERMARKET</a>
+ 
+  <div class='header-right'>
+    <a  href='index.php'>SHOW TABLES</a>
+    <a class='active' href='insert.php'>Insert</a>
+  </div>
+</div>
+
+<div class="container">
+<h2 >Insert Product </h2>
+<br>
+ <form name="myForm"  method="post" action = "product.php" method = "post">
+ <div class="form-group">
+    <label>Product ID</label>
+    <input type="text" class="form-control" name="productId"  placeholder="Product Id">
+    <small id="" class="form-text text-muted"></small>
+  </div>
+ <div class="form-group">
+    <label for="exampleInputEmail1">Product Name</label>
+    <input type="text" class="form-control" name="productName"  placeholder="Product Name">
+    <small id="emailHelp" class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Brand Name</label>
+    <input type="text" class="form-control" name="brandName" placeholder="Brand Name">
+    <small id="" class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Department Name</label>
+    <select type="text" class="form-control" name="departmentName" placeholder="Department Name">
+      <option value = "Food Section"> Food Section </option> 
+      <option value = "Household"> Household </option>
+      <option value = "Sports"> Sports </option>
+      <option value = "Electronics"> Electronics </option>
+      <option value = "Clothes"> Clothes </option>
+    </select>
+    <small id="" class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="">CostPrice</label>
+    <input type="text" class="form-control" name="costPrice" placeholder="costPrice">
+    <small id="" class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="">MRP</label>
+    <input type="text" class="form-control" name="mrp" placeholder="MRP">
+    <small id="" class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="">Quantity in stock</label>
+    <input type="text" class="form-control" name="quantity" placeholder="Quantity">
+    <small id="" class="form-text text-muted"></small>
+  </div>
+      <button type="submit" class="btn btn-primary" >Submit</button>
+      
+      <?php
+  require('config.php');
+ 
+    // If form submitted, insert values into the database.
+    if (isset($_REQUEST['productId'])){
+		$productId = stripslashes($_REQUEST['productId']); // removes backslashes
+    $productId= mysqli_real_escape_string($con,$productId); //escapes special characters in a string
+		$productName = stripslashes($_REQUEST['productName']);
+    $productName = mysqli_real_escape_string($con,$productName);
+		$brandName = stripslashes($_REQUEST['brandName']);
+    $brandName = mysqli_real_escape_string($con,$brandName);
+
+    $departmentName = stripslashes($_REQUEST['departmentName']);
+    $departmentName = mysqli_real_escape_string($con,$departmentName);
+
+    $costPrice = stripslashes($_REQUEST['costPrice']);
+    $costPrice = mysqli_real_escape_string($con,$costPrice);
+
+    $mrp = stripslashes($_REQUEST['mrp']);
+    $mrp = mysqli_real_escape_string($con,$mrp);
+
+    $quantity = stripslashes($_REQUEST['quantity']);
+    $quantity = mysqli_real_escape_string($con,$quantity);
+
+      
+    
+    
+  	$query = "INSERT into `product` (productID,productName,brandName,departmentName,costPrice,MRP,quantityStock) VALUES ('$productId','$productName','$brandName', '$departmentName','$costPrice','$mrp','$quantity')";
+    $result = mysqli_query($con,$query);
+        if($result){
+          echo "success";
+          session_unset();
+          } 
+          else 
+          {
+            echo "fail";
+          }
+      
+      }
+   ?>
